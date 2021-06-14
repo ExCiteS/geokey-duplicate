532c578234d0e751c9ae2bd970b4cfdb3bf28331
Francisco Sanz <frasanz@bifi.es>
2021-06-14 05:48:26 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -9,19 +9,19 @@ def read(file_name):
         return file_object.read()
 
 
-name = 'geokey-export'
+name = 'geokey-duplicate'
 version = __import__(name.replace('-', '_')).__version__
 repository = join('https://github.com/ExCiteS', name)
 
 setup(
     name=name,
     version=version,
-    description='Export data from GeoKey to KML, GeoJSON, CSV',
+    description='Duplicate projects and categories',
     long_description=read('README.rst'),
     url=repository,
     download_url=join(repository, 'tarball', version),
-    author='ExCiteS',
-    author_email='excites@ucl.ac.uk',
+    author='Francisco Sanz',
+    author_email='frasanz@bifi.es',
     license='MIT',
     packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*']),
     include_package_data=True,
