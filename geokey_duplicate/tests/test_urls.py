31a649beec0748f97185fbd52a24437b5335d8ec
Francisco Sanz <frasanz@bifi.es>
2021-06-14 05:36:38 +0000

    first commit

---
+++ geokey_duplicate/tests/test_urls.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse, resolve
+
+from ..views import (
+    IndexPage, ExportOverview, ExportCreate, ExportDelete,
+    ExportGetProjectCategories, ExportGetProjectCategoryContributions,
+    ExportGetExportContributions, ExportToRenderer
+)
+
+
+class UrlTest(TestCase):
+
+    def test_index_page(self):
+        self.assertEqual(reverse('geokey_duplicate:index'), '/admin/duplicate/')
+
+        resolved = resolve('/admin/duplicate/')
+        self.assertEqual(resolved.func.func_name, IndexPage.__name__)
